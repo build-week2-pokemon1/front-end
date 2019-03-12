e830cb96db11cdc339eaa5cbabbe90349813c922
UnknownMonk <smodcartoon@gmail.com>
2019-03-12 15:44:22 -0400

    Compare done style done

--- pokemon/src/components/PokemonList.js
+++ pokemon/src/components/PokemonList.js
@@ -3,13 +3,18 @@ import { connect } from 'react-redux';
 import SearchBar from './SearchBar';
 import Pokemon from './Pokemon';
 import { pokeSelect, pokeCompare } from '../actions';
+import styled from 'styled-components';
+import Compare from '../img/compare.png';
+import Pokeball from '../img/Pokeball.png';
 
 const PokemonList = props => {
   console.log('SELECTED', props);
   return (
     <>
       <SearchBar />
-      <button onClick={props.pokeCompare}>Compare Pokemon</button>
+      <Button1 onClick={props.pokeCompare}>
+        <Compare1 src={Compare} alt="Compare" />
+      </Button1>
       <div className="pokemonContainer">
         {props.filtered.length !== 0
           ? props.filtered.map(poke => (
@@ -28,6 +33,22 @@ const mapStateToProps = state => ({
   selected: state.pokemonReducer.selected,
   filtered: state.pokemonReducer.filtered
 });
+
+const Button1 = styled.button`
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  border-style: none;
+  background-image: url(${Pokeball});
+  background-size: cover;
+  background-color: white;
+  margin: 10px;
+  cursor: pointer;
+`;
+
+const Compare1 = styled.img`
+  width: 200px;
+`;
 
 export default connect(
   mapStateToProps,
--- pokemon/src/components/SearchBar.js
+++ pokemon/src/components/SearchBar.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { searchPokemon } from '../actions';
+import styled from 'styled-components';
 
 const SearchBar = props => {
   const [search, setSearch] = useState('');
 
   return (
     <form>
-      <input
+      <Input1
         type="text"
         onChange={e => {
           e.preventDefault();
@@ -24,6 +25,18 @@ const SearchBar = props => {
 const mapStateToProps = state => ({
   pokemon: state.pokemonReducer.pokemon
 });
+
+const Input1 = styled.input`
+  padding-top: 30px;
+  border-style: none;
+  font-size: 30px;
+  font-weight: bold;
+
+  text-align: center;
+  margin: 0 auto;
+
+  border-bottom: 1px solid black;
+`;
 
 export default connect(
   mapStateToProps,
