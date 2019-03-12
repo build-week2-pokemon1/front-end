548027ff41c8fca73fe14927c71d8a44111f67b8
korynewton <korynewtondev@gmail.com>
2019-03-11 19:57:03 -0700

    pokemon component working

---
+++ pokemon/src/components/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokemon = props => {
+    console.log(props.pokemon)
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Pokemon
--- pokemon/src/components/PokemonList.js
+++ pokemon/src/components/PokemonList.js
@@ -1,32 +1,15 @@
 import React from "react";
 import { connect } from 'react-redux'
+import Pokemon from './Pokemon'
+
 
 
 const PokemonList = props => {
-  console.log('poke props:', props.pokemon)
   return (
-    <ul>
-      {props.pokemon.map(poke => {
-        return (
-            <ul key={poke.Name}>
-                <li>Name: {poke.Name}</li>
-                <li>Type 1: {poke["Type 1"]}</li>
-                <li>Type 2: {poke["Type 2"]}</li>
-                <li>Total: {poke.Total}</li>
-                <li>HP: {poke.HP}</li>
-                <li>Attack: {poke.Attack}</li>
-                <li>Defense: {poke.Defense}</li>
-                <li>Special Attack: {poke["Sp Atk"]}</li>
-                <li>Special Defense: {poke["Sp Def"]}</li>
-                <li>Speed: {poke.Speed}</li>
-                <li>Generation: {poke.Generation}</li>
-                <li>Legendary: {poke.Legendary}</li>             
-            </ul>
-                )
-            })}
-    </ul>
-  );
-};
+    props.pokemon.map(poke => <Pokemon key={poke.name} pokemon={poke} />)
+  )
+}
+        
 
 const mapStateToProps = state => ({
     pokemon : state.pokemonReducer.pokemon
